5b6e9ed8f7218bf78bfdc8c355af502a96a64a3a
jiapeng tong <jiapengt@student.unimelb.edu.au>
2020-04-05 17:49:37 +0800

    integrate code add single core mode

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from mpi4py import MPI
 import numpy as np
 import json
 from collections import Counter
+import string
+import subprocess
 
 
 # function to handle json language code
@@ -13,46 +15,112 @@ def handleJSONLanuageCode(dataObj):
     else:
         languages_list[lang_code] = 1
 
+
 # function to handle string language code
 def handleStringLanguageCode(str):
+    keyword = 'iso_language_code'
+    start_index = str.find(keyword)
+    lang_code_start_index = 0
+    lang_code_end_index = 0
+    flag = 0
+    if start_index != -1:
+        for i in range(start_index + len(keyword) + 1, len(str)):
+            if flag == 0:
+                if str[i] == '\"':
+                    lang_code_start_index = i
+                    flag += 1
+            elif flag == 1:
+                if str[i] == '\"':
+                    lang_code_end_index = i
+                    flag += 1
+            elif flag == 2:
+                break
+    lang_code = str[lang_code_start_index + 1: lang_code_end_index]
+    if lang_code in languages_list:
+        languages_list[lang_code] += 1
+    else:
+        languages_list[lang_code] = 1
+
+
+# function to handle json Hashtag code
+def handleJSONHashtagCode(dataObj):
+    hashtags = dataObj['doc']['entities']['hashtags']
+    text_tags = dataObj['doc']['text']
+
+    # takes all the hashtags in the 'hashtag' sub-section
+    for tag in hashtags:
+        hashtag = str(tag['text']).lower()
+        if hashtag in hashtags_list:
+            hashtags_list[hashtag] += 1
+        else:
+            hashtags_list[hashtag] = 1
+
+    # takes all hashtags in the 'text' sub-section
+    text_tags = [tag.strip("#").lower() for tag in text_tags.replace('#', ' #').split() if tag.startswith("#")]
+    if text_tags:
+        for tag in text_tags:
+            # remove punctuation
+            for punc in string.punctuation:
+                if punc in tag:
+                    tag = tag.replace(punc, '')
+
+            if tag in hashtags_list:
+                hashtags_list[tag] += 1
+            else:
+                hashtags_list[tag] = 1
+
+
+# function to handle string Hashtag code
+def handleStringHashtagCode(str):
     return None
-    # print("search all str to find if there are language code")
+    # print("search all str to find if there are hashtag code")
 
 
 comm = MPI.COMM_WORLD
 size = comm.size
 rank = comm.rank
 
-hashtags = {}           # hashtag dict
-languages_list = {}     # languages dict
-my_size = 0             # size the current process should read
-start_lines = 0         # start point of current process
+hashtags_list = {}              # hashtag dict
+languages_list = {}             # languages dict
+my_size = 0                     # size the current process should read
+start_lines = 0                 # start point of current process
+isSingleCoreMode = False        # using single core mode or not
+file_name = 'bigTwitter.json'   # file name
 
-# amode = MPI.MODE_WRONLY|MPI.MODE_CREATE
-# f = MPI.File.Open(comm, "tinyTwitter.json", amode)
-file = open("tinyTwitter.json")
-# get total rows in the file (not include 1st row)
-# total_lines = int(file.readline().split(",")[0].split(':')[1])
+# determine the mode
+if size == 1:
+    isSingleCoreMode = True
+else:
+    isSingleCoreMode = False
+
 # calculate the total lines
 if rank == 0:
-    total_lines = 0
-    for index, line in enumerate(file):
-        total_lines = index + 1
+    out = subprocess.run(['wc', '-l', file_name],
+                         stdout=subprocess.PIPE).stdout.decode('utf-8')
+    total_lines = int(out.strip().split()[0])
 else:
     total_lines = 0
-# when broadcast rank 0 does not work on the task
-total_lines = comm.bcast(total_lines, root=0)
-# default size a process should read
-default_size = np.ceil(total_lines / (size - 1))
-# the size that last process should read
-end_size = total_lines - (default_size * (size - 2))
-# assign the size that current process should read
-for i in range(0, size):
-    if rank == size - 1:
-        my_size = end_size
-    if rank == i:
-        my_size = default_size
-    start_lines = default_size * (rank - 1)    # assign different start lines to each process
+
+file = open(file_name)
+
+if not isSingleCoreMode:
+    # when broadcast rank 0 does not work on the task
+    total_lines = comm.bcast(total_lines, root=0)
+    # default size a process should read
+    default_size = np.ceil(total_lines / (size - 1))
+    # the size that last process should read
+    end_size = total_lines - (default_size * (size - 2))
+    # assign the size that current process should read
+    for i in range(0, size):
+        if rank == size - 1:
+            my_size = end_size
+        if rank == i:
+            my_size = default_size
+
+        start_lines = default_size * (rank - 1)    # assign different start lines to each process
+else:
+    my_size = total_lines
+
 # read the file from start point
 for index, line in enumerate(file):
     if index == int(start_lines):
@@ -67,22 +135,29 @@ for index, line in enumerate(file):
                     validJSON = False
                 if validJSON:
                     # count hashtag contents
-                    # TODO write a new function to add up hashtags
+                    handleJSONHashtagCode(dataObj)
                     # count language code
                     handleJSONLanuageCode(dataObj)
                 else:
                     handleStringLanguageCode(dataStr)
         break       # does not allow each process go through the whole file
 file.close()
-
-# sort hashtag list based on appear times
-# TODO sort hashtag list based on each value
-# sort language list base on appear times
 comm.Barrier()
-collect_result = comm.gather(languages_list, root=0)
+
+collect_result_language = comm.gather(languages_list, root=0)
+collect_result_hashtags = comm.gather(hashtags_list, root=0)
+
 if rank == 0:
-    final_result = Counter({})
-    for element in collect_result:
-        final_result += Counter(element)
-    print(sorted(final_result.items(), key=lambda x: x[1], reverse=True)[0:10])
+    # language
+    final_result_language = Counter({})
+    for element in collect_result_language:
+        final_result_language += Counter(element)
+
+    # hashtag
+    final_result_hashtags = Counter({})
+    for hashtag_element in collect_result_hashtags:
+        final_result_hashtags += Counter(hashtag_element)
+
+    print(sorted(final_result_language.items(), key=lambda x: x[1], reverse=True)[0:10])
+    print(sorted(final_result_hashtags.items(), key=lambda x: x[1], reverse=True)[0:10])
 
