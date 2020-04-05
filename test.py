129c9ba38542f68c45e39dfbd0a2be1306b65e46
Marc Girard <girard.marc07@hotmail.fr>
2020-04-05 16:48:20 +1000

    update test with hashtags

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 # !usr/bin/env python
+
+# on local dir, run cmd: mpirun -np 4 python3 test.py
+
 from mpi4py import MPI
 import numpy as np
 import json
@@ -8,12 +11,12 @@ comm = MPI.COMM_WORLD
 size = comm.size
 rank = comm.rank
 
-hashtags = {}           # hashtag dict
+hashtags_List = {}           # hashtag dict
 languages_list = {}     # languages dict
 my_size = 0             # size the current process should read
 start_lines = 0         # start point of current process
 
-file = open("tinyTwitter.json")
+file = open("tinyTwitterV2.json")
 # get total rows in the file (not include 1st row)
 total_lines = int(file.readline().split(",")[0].split(':')[1])
 # default size a process should read
@@ -28,16 +31,28 @@ for i in range(0, size):
         my_size = default_size
     start_lines = default_size * rank    # assign different start lines to each process
 # read the file from start point
+itera = 0
+itera += 0
 for index, line in enumerate(file):
     if index == int(start_lines):
         for j in range(0, int(my_size)):
             dataStr = file.readline()
             if dataStr != '':
-                dataObj = json.loads(dataStr[: -2])     # remove \n and ,
+                dataObj = json.loads(dataStr[:-2])     # remove \n and ,
                 # count hashtag contents
-                # TODO write a new function to add up hashtags
+                # TODO write a new function to add up hashtags_List
+                hashtag_code = dataObj['doc']['entities']['hashtags']
+
+                if hashtag_code:
+                    hashtag_code = str(hashtag_code[0]['text'])
+                    if hashtag_code in hashtags_List:
+                        hashtags_List[hashtag_code] += 1
+                    else:
+                        hashtags_List[hashtag_code] = 1
+
                 # count language code
                 lang_code = dataObj['doc']['metadata']['iso_language_code']
+
                 if lang_code in languages_list:
                     languages_list[lang_code] += 1
                 else:
@@ -49,10 +64,21 @@ file.close()
 # TODO sort hashtag list based on each value
 # sort language list base on appear times
 comm.Barrier()
-collect_result = comm.gather(languages_list, root=0)
+collect_result_language = comm.gather(languages_list, root=0)
+collect_result_hashtags = comm.gather(hashtags_List, root=0)
+
 if rank == 0:
-    final_result = Counter({})
-    for element in collect_result:
-        final_result += Counter(element)
+    # language
+    final_result_language = Counter({})
+    for element in collect_result_language:
+        final_result_language += Counter(element)
+    
+    #hashtag
+    final_result_hashtags = Counter({})
+    for hashtag_element in collect_result_hashtags:
+        final_result_hashtags += Counter(element)
+
     # print(final_result)
-    print(sorted(final_result.items(), key=lambda x: x[1], reverse=True)[0:9])
+    print(sorted(final_result_language.items(), key=lambda x: x[1], reverse=True)[0:9])
+    print(sorted(final_result_hashtags.items(), key=lambda x: x[1], reverse=True)[0:9])
+
