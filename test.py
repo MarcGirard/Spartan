06effb349320e34ae1c0054ac7fdf3fa2fb00391
Marc Girard <girard.marc07@hotmail.fr>
2020-04-05 18:00:27 +1000

    update hashtags inside twitter text

--- test.py
+++ test.py
@@ -6,6 +6,54 @@ from mpi4py import MPI
 import numpy as np
 import json
 from collections import Counter
+import string
+
+# function to handle json language code
+def handleJSONLanuageCode(dataObj):
+    lang_code = dataObj['doc']['metadata']['iso_language_code']
+    if lang_code in languages_list:
+        languages_list[lang_code] += 1
+    else:
+        languages_list[lang_code] = 1
+
+# function to handle string language code
+def handleStringLanguageCode(str):
+    return None
+    # print("search all str to find if there are language code")
+
+
+# function to handle json Hashtag code
+def handleJSONHashtagCode(dataObj):
+    hashtag_code = dataObj['doc']['entities']['hashtags']
+    text_tags = dataObj['doc']['text']
+
+    #takes all the hashtags in the 'hashtag' sub-section
+    if hashtag_code:
+        hashtag_code = str(hashtag_code[0]['text']).lower()
+        if hashtag_code in hashtags_List:
+            hashtags_List[hashtag_code] += 1
+        else:
+            hashtags_List[hashtag_code] = 1
+
+    #takes all hashtags in the 'text' sub-section
+    text_tags = [tag.strip("#").lower() for tag in text_tags.replace('#',' #').split() if tag.startswith("#")]
+    if text_tags:
+        for tag in text_tags:
+            #remove punctuation
+            for punc in string.punctuation:
+                if punc in tag:
+                    tag = tag.replace(punc,'')
+
+            if tag in hashtags_List:
+                hashtags_List[tag] += 1
+            else:
+                hashtags_List[tag] = 1
+    
+# function to handle string Hashtag code
+def handleStringHashtagCode(str):
+    return None
+    # print("search all str to find if there are hashtag code")
+
 
 comm = MPI.COMM_WORLD
 size = comm.size
@@ -16,13 +64,24 @@ languages_list = {}     # languages dict
 my_size = 0             # size the current process should read
 start_lines = 0         # start point of current process
 
-file = open("tinyTwitterV2.json")
+# amode = MPI.MODE_WRONLY|MPI.MODE_CREATE
+# f = MPI.File.Open(comm, "tinyTwitter.json", amode)
+file = open("tinyTwitter.json")
 # get total rows in the file (not include 1st row)
-total_lines = int(file.readline().split(",")[0].split(':')[1])
+# total_lines = int(file.readline().split(",")[0].split(':')[1])
+# calculate the total lines
+if rank == 0:
+    total_lines = 0
+    for index, line in enumerate(file):
+        total_lines = index + 1
+else:
+    total_lines = 0
+# when broadcast rank 0 does not work on the task
+total_lines = comm.bcast(total_lines, root=0)
 # default size a process should read
-default_size = np.ceil(total_lines / size)
+default_size = np.ceil(total_lines / (size - 1))
 # the size that last process should read
-end_size = total_lines - (default_size * (size - 1))
+end_size = total_lines - (default_size * (size - 2))
 # assign the size that current process should read
 for i in range(0, size):
     if rank == size - 1:
@@ -31,33 +90,28 @@ for i in range(0, size):
         my_size = default_size
     start_lines = default_size * rank    # assign different start lines to each process
 # read the file from start point
-itera = 0
-itera += 0
+
 for index, line in enumerate(file):
     if index == int(start_lines):
         for j in range(0, int(my_size)):
             dataStr = file.readline()
+            validJSON = True
             if dataStr != '':
-                dataObj = json.loads(dataStr[:-2])     # remove \n and ,
-                # count hashtag contents
-                # TODO write a new function to add up hashtags_List
-                hashtag_code = dataObj['doc']['entities']['hashtags']
+                dataObj = {}
+                try:
+                    dataObj = json.loads(dataStr[: -2])     # remove \n and ,
+                except ValueError as e:
+                    validJSON = False
+                if validJSON:
+                    # count hashtag contents
+                    handleJSONHashtagCode(dataObj)
+                    # count language code
+                    handleJSONLanuageCode(dataObj)
+                # else:
+                #     handleStringHashtagCode(dataStr)
+                #     handleStringLanguageCode(dataStr)
+        break       # does not allow each process go through the whole file
 
-                if hashtag_code:
-                    hashtag_code = str(hashtag_code[0]['text'])
-                    if hashtag_code in hashtags_List:
-                        hashtags_List[hashtag_code] += 1
-                    else:
-                        hashtags_List[hashtag_code] = 1
-
-                # count language code
-                lang_code = dataObj['doc']['metadata']['iso_language_code']
-
-                if lang_code in languages_list:
-                    languages_list[lang_code] += 1
-                else:
-                    languages_list[lang_code] = 1
-        break                   # does not allow each process go through the whole file
 file.close()
 
 # sort hashtag list based on appear times
@@ -76,9 +130,10 @@ if rank == 0:
     #hashtag
     final_result_hashtags = Counter({})
     for hashtag_element in collect_result_hashtags:
-        final_result_hashtags += Counter(element)
+        final_result_hashtags += Counter(hashtag_element)
 
     # print(final_result)
-    print(sorted(final_result_language.items(), key=lambda x: x[1], reverse=True)[0:9])
-    print(sorted(final_result_hashtags.items(), key=lambda x: x[1], reverse=True)[0:9])
+    print(sorted(final_result_language.items(), key=lambda x: x[1], reverse=True)[0:10])
+    print(sorted(final_result_hashtags.items(), key=lambda x: x[1], reverse=True)[0:10])
+
 
